23e05c5d5c0437fc845746c9d81bf64bd66db950
Tim Kramer <noreply@example.com>
2023-12-04 21:24:52 +0100

    Add day 4 solutions

---
+++ day-04.rkt
@@ -0,0 +1,187 @@
+#lang racket
+
+; A card is represented as a pair, consisting of a number and a pair
+; of sets of numbers. The number is the identifier of the card. The
+; first set of the pair of sets is the winning set. The second pair is
+; the found set.
+(define (make-card id win found)
+  (cons id (cons win found)))
+
+; An abbrev is a pair of a symbol and a string to match. The string is
+; the key, so this comes first in the representation.
+(define (make-abbrev symbol str)
+  (cons str symbol))
+
+; The token list defines how to translate the data format to symbols
+(define token-list
+  (list
+   (make-abbrev 'number-sep " ")
+   (make-abbrev '0 "0")
+   (make-abbrev '1 "1")
+   (make-abbrev '2 "2")
+   (make-abbrev '3 "3")
+   (make-abbrev '4 "4")
+   (make-abbrev '5 "5")
+   (make-abbrev '6 "6")
+   (make-abbrev '7 "7")
+   (make-abbrev '8 "8")
+   (make-abbrev '9 "9")
+   (make-abbrev 'game-start "Card")
+   (make-abbrev 'id-sep ":")
+   (make-abbrev 'winset-sep (regexp-quote "|"))
+   (make-abbrev 'cr "\r")
+   (make-abbrev 'nl "\n")))
+
+; A regex that matches token-list
+(define token-regex
+  (regexp (string-join (list "(" (string-join (dict-keys token-list) "|") ")") "")))
+
+; Match token string to symbol
+(define (token-match str)
+  (dict-ref token-list str))
+
+; function that converts the input file into a symbol list
+(define (lexer in)
+  (cond
+    [(eof-object? (peek-char in)) '(#f)]
+    [else (list (token-match (regexp-quote (bytes->string/utf-8 (car (regexp-match token-regex in))))))]))
+
+(define (lexer-peek in)
+  (cond
+    [(eof-object? (peek-char in)) '(#f)]
+    [else (list (token-match (bytes->string/utf-8 (car (regexp-match-peek token-regex in)))))]))
+
+(define (parse stack in)
+  (cond
+    ; If the stack is empty, put something on there
+    [(null? stack) (parse (cons (lexer in) stack) in)]
+    ; If the top value is '(#f) we reached eof and return the parse
+    [(false? (caar stack)) (cdr stack)]
+    ; Convert to numbers
+    [(or
+      (eq? (caar stack) '0)
+      (eq? (caar stack) '1)
+      (eq? (caar stack) '2)
+      (eq? (caar stack) '3)
+      (eq? (caar stack) '4)
+      (eq? (caar stack) '5)
+      (eq? (caar stack) '6)
+      (eq? (caar stack) '7)
+      (eq? (caar stack) '8)
+      (eq? (caar stack) '9))
+     (parse (cons (cons 'number (number->string (caar stack))) (cdr stack)) in)]
+    ; Combine sequential numbers to number
+    [(and (not (null? (cdr stack))) (eq? (caar stack) 'number) (eq? (caadr stack) 'number))
+     (parse (cons (cons 'number (string-append (cdadr stack) (cdar stack))) (cddr stack)) in)]
+    ; Replace \r\n or \n to game-end
+    [(and (not (null? (cdr stack))) (eq? (caar stack) 'nl) (eq? (caadr stack) 'cr))
+     (parse (cons '(game-end) (cddr stack)) in)]
+    [(and (not (null? (cdr stack))) (eq? (caar stack) 'nl) (not (eq? (caadr stack) 'cr)))
+     (parse (cons '(game-end) (cddr stack)) in)]
+    ; remove superfluous number-seps
+    [(and (not (null? (cdr stack))) (eq? (caar stack) 'number-sep) (not (eq? (caadr stack) 'number)))
+     (parse (cdr stack) in)]
+    ; Find identifier
+    [(and (not (null? (cdr stack))) (eq? (caar stack) 'id-sep) (eq? (caadr stack) 'number))
+     (parse (cons (cons 'identifier (cdadr stack)) (cddr stack)) in)]
+    ; Create winset
+    [(and
+      (not (null? (cdr stack)))
+      (not (null? (cddr stack)))
+      (eq? (caar stack) 'winset-sep)
+      (eq? (caadr stack) 'number-sep)
+      (eq? (caaddr stack) 'number))
+     (parse (cons (cons 'winset (list (cdaddr stack))) (cdddr stack)) in)]
+    ; Add to winset
+    [(and
+      (not (null? (cdr stack)))
+      (not (null? (cddr stack)))
+      (eq? (caar stack) 'winset)
+      (eq? (caadr stack) 'number-sep)
+      (eq? (caaddr stack) 'number))
+     (parse (cons (cons 'winset (cons (cdaddr stack) (cdar stack))) (cdddr stack)) in)]
+    ; Create foundset
+    [(and
+      (not (null? (cdr stack)))
+      (eq? (caar stack) 'game-end)
+      (eq? (caadr stack) 'number))
+     (parse (cons (cons 'foundset (list (cdadr stack))) (cddr stack)) in)]
+    ; Add to foundset
+    [(and
+      (not (null? (cdr stack)))
+      (not (null? (cddr stack)))
+      (eq? (caar stack) 'foundset)
+      (eq? (caadr stack) 'number-sep)
+      (eq? (caaddr stack) 'number))
+     (parse (cons (cons 'foundset (cons (cdaddr stack) (cdar stack))) (cdddr stack)) in)]
+    ; Create game
+    [(and
+      (not (null? (cdr stack)))
+      (not (null? (cddr stack)))
+      (not (null? (cdddr stack)))
+      (eq? (caar stack) 'foundset)
+      (eq? (caadr stack) 'winset)
+      (eq? (caaddr stack) 'identifier)
+      (eq? (car (cadddr stack)) 'game-start))
+     (parse (cons (cons 'game (cons (cdaddr stack) (cons (cdadr stack) (cdar stack)))) (cddddr stack)) in)]
+    ; Put the next item on the stack
+    [else (parse (cons (lexer in) stack) in)]))
+
+(define in-test (open-input-file "day-04-input-test.txt"))
+(define in (open-input-file "day-04-input.txt"))
+(define parsetree (parse '() in))
+(define card-list
+  (map
+   (λ (game)
+     (make-card
+      (string->number (cadr game))
+      (list->set (map string->number (caddr game)))
+      (list->set (map string->number (cdddr game)))))
+   parsetree))
+
+; Part 1 solution
+(eval
+ (cons
+  '+
+  (map
+   (λ (card)
+     (if
+      (eq? (set-count (set-intersect (cadr card) (cddr card))) 0)
+      0
+      (expt 2 (- (set-count (set-intersect (cadr card) (cddr card))) 1))))
+   card-list)))
+
+; Part 2 solution
+
+; Find the number of wins for each card
+(define (find-wins cards)
+  (map (λ (card) (cons (car card) (set-count (set-intersect (cadr card) (cddr card))))) cards))
+
+; Find wins per card ID
+(define win-hash (make-hash (find-wins card-list)))
+(define (find-wins-per-id card-ids)
+  (map (λ (card-id) (cons card-id (dict-ref win-hash card-id))) card-ids))
+
+; Find the new cards-ids on card list cl from win
+(define number-of-cards (length card-list))
+(define (find-new-card-ids acc win)
+  (cond
+    [(eq? (cdr win) 0) acc]
+    [(> (+ (car win) (cdr win)) number-of-cards)
+     (find-new-card-ids number-of-cards acc (cons (car win) (- (cdr win) 1)))]
+    [else (find-new-card-ids
+           (cons (+ (car win) (cdr win)) acc)
+           (cons (car win) (- (cdr win) 1)))]))
+
+; Find the cards for the next round
+(define (find-new-cards-per-id cil)
+  (flatten
+   (map (λ (win) (find-new-card-ids '() win)) (find-wins-per-id cil)))) 
+
+; Play game per id
+(define (play-game-per-id acc cil)
+  (cond
+    [(null? cil) acc]
+    [else (play-game-per-id (+ (length cil) acc) (find-new-cards-per-id cil))]))
+
+(play-game-per-id 0 (map car card-list))
